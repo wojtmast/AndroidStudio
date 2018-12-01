b48ad20a79c851923d6414b1c9437b2ac52a4f41
wojtmast <w.l.leszczynski@gmail.com>
2018-12-01 23:22:53 +0100

    Postawiona i działająca apka

---
+++ app/src/main/java/com/example/wlles/sqliteoperations/Message.java
@@ -0,0 +1,9 @@
+package com.example.wlles.sqliteoperations;
+import android.content.Context;
+import android.widget.Toast;
+
+public class Message {
+    public static void message(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+}
